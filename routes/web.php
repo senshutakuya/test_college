0b4f4e124d8ca2a93cd55ddbdd99a0066d83e587
senshutakuya <21c1100008st@edu.tech.ac.jp>
2023-08-05 18:14:27 +0900

    Laravelの環境設定をコミットする

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+
+
+   
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index']);
